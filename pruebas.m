7a075c90b8800fafe6c08769025c1d259ba98e4b
LorenzoCanoAn <736078@unizar.es>
2021-04-18 21:20:08 +0200

    "terminado 2!

---
+++ pruebas.m
@@ -0,0 +1,6 @@
+%% Ejecución
+
+fichero_cif="data/4BS_X_RAYS_ICSD_87927/ICSD_CollCode87927.cif";
+fichero_vesta="data/4BS_X_RAYS_ICSD_87927/ICSD_CollCode87927_VESTA_dmin_1.txt";
+
+hola = parameters_for_structure_factor(fichero_cif, fichero_vesta);
